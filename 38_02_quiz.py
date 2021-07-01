6b1381422fc53645e3a512fc5f4a120277258ddc
asg0416 <73174694+asg0416@users.noreply.github.com>
2021-07-01 17:38:48 +0900

    38: 터렛 반지름 비교

--- 38_02_quiz.py
+++ 38_02_quiz.py
@@ -4,6 +4,7 @@ n = int(stdin.readline())
 for _ in range(n):
     x1, y1, r1, x2, y2, r2 = map(int, stdin.readline().strip().split())
     r = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+    
     if r == 0 and r1 == r2:
         print(-1)
     elif abs(r1 - r2) == r or r1 + r2 == r:
